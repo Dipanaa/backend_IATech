fe425a1c1ad1817fae8bfea3b55e1b6e6470e577
dotDipana <dguerrajoselin@gmail.com>
2025-12-15 11:12:42 -0300

    Commit inicial con backend creado con Flask

---
+++ ia_model/quality_life_model.py
@@ -0,0 +1,43 @@
+
+#Importacion de librerias
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+
+df = pd.read_csv("data/Mental_Health_and_Social_Media_Balance_Dataset.csv")
+
+df.rename(columns={
+    'Stress_Level(1-10)': 'Nivel de Estres',
+    'Sleep_Quality(1-10)': 'Calidad del Sueño',
+    'Daily_Screen_Time(hrs)': 'Tiempo en Pantalla',
+
+}, inplace=True)
+
+df['Estado de Salud'] = np.where(
+    (df['Nivel de Estres'] > 6) |
+    (df['Calidad del Sueño'] < 4)|
+    (df['Tiempo en Pantalla'] > 6),
+    0,
+    1
+)
+
+X_training = df[['Nivel de Estres', 'Calidad del Sueño', 'Tiempo en Pantalla']]
+y_training = df['Estado de Salud']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_training, y_training, test_size=0.2, random_state=42, stratify=y_training
+)
+
+model = RandomForestClassifier(
+    n_estimators = 100,
+    max_depth = None,
+    random_state = 42
+)
+model.fit(X_train, y_train)
+
+joblib.dump(model, 'modelo_entrenado.pkl')
---
+++ main.py
@@ -0,0 +1,88 @@
+from flask import Flask, jsonify, request
+import joblib
+import numpy as np
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+#Creacion de la instancia
+app = Flask(__name__)
+
+#Cargar entorno gemini
+load_dotenv(dotenv_path="environment/developer.env")
+
+#gemini key
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if gemini_api_key is None:
+    print("Clave no encontrada")
+
+
+#Carga de modelo IA
+try:
+    # Rutra de modelo pkl en binario
+    RUTA_MODELO = 'modelo_entrenado.pkl' 
+    modelo_ia = joblib.load(RUTA_MODELO)
+    print("Modelo de IA cargado con éxito en memoria.")
+except Exception as e:
+    print(f"ERROR: No se pudo cargar el modelo. ¿Está en la ruta correcta? {e}")
+    modelo_ia = None
+
+#Endpoints
+#POST predict endpoint 
+@app.route("/predict",methods=["POST"])
+async def predict():
+    if modelo_ia is None:
+        return jsonify({"error": "Modelo no disponible, el servidor falló al cargarlo."}), 503
+
+    # Datos recibidos por json
+    data = request.json
+    
+    # Validación y Extracción de datos
+    try:
+        input_data = np.array([
+            data['Nivel_Estres'], 
+            data['Calidad_Sueno'], 
+            data['Tiempo_Pantalla']
+        ]).reshape(1, -1) # Formato para construccion de array 2D con filas y columnas
+    except KeyError:
+        return jsonify({"error": "Faltan datos de entrada ('Nivel_Estres', 'Calidad_Sueno', 'Tiempo_Pantalla')."}), 400
+
+    # 2. Predicción
+    prediccion = modelo_ia.predict(input_data)[0]
+    
+    # 3. Formateo de la respuesta
+    resultado = "Saludable" if prediccion == 1 else "No Saludable"
+
+    #Entregar resultados con gemini
+
+    model = genai.GenerativeModel("gemini-2.5-flash")
+
+    #Prompt de analisis
+
+    prompt = f"""
+    Tu tarea como modelo es entregar recomendaciones de salud digital que deben ser solo 3 en base a
+    la respuesta que entrege un modelo predictivo. Esta puede ser Saludable o No Saludable.
+    Solo debes entregar recomendaciones si la respuesta es No Saludable si no debes decir lo siguiente:
+    Usted presenta buenos habitos de salud digital, por favor continue de esa forma.
+
+    RESPUESTA : {resultado}
+
+    No debes entregar informacion fuera del contexto de salud digital
+    """
+
+
+    try:
+        response = await model.generate_content_async(prompt)
+    except:
+        return jsonify({"error": "Modelo de Gemini no disponible por sobrecarga, el servidor falló al cargarlo."}), 503
+
+    return jsonify({
+        "prediccion_numerica": int(prediccion),
+        "estado_salud": resultado,
+        "recomendacion_ia": response.text
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
